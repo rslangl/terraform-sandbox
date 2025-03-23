9404955ef7f102b23c8a29acee42a3354206da37
rslangl <fjellape1447@protonmail.com>
2025-03-23 17:12:03 +0100

    feat: add cloud init logic

--- modules/dev/main.tf
+++ modules/dev/main.tf
@@ -1,3 +1,32 @@
+resource "proxmox_virtual_environment_file" "vm_cloud_config" {
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name = "wintermute"
+
+  source_raw {
+    data = <<-EOF
+    # cloud-config
+    users:
+    - name: herder
+      groups:
+        - sudo
+      shell: /bin/bash
+      ssh_authorized_keys:
+      sudo: ALL=(ALL) NOPASSWD:ALL
+    package_update: true
+    packages:
+      - qemu-guest-agent
+      - net-tools
+    runcmd:
+      - systemctl enable qemu-guest-agent
+      - systemctl start qemu-guest-agent
+      - echo "done" > /tmp/cloud-config.done
+    EOF
+
+    file_name = "alpine-config.yaml"
+  }
+}
+
 resource "proxmox_virtual_environment_vm" "vm" {
   name = "dev"
   node_name = "wintermute"
@@ -35,6 +64,8 @@ resource "proxmox_virtual_environment_vm" "vm" {
       password = random_password.vm_password.result
       username = "herder"
     }
+    #vendor_data_file_id = proxmox_virtual_environment_file.vm_cloud_config.id
+
   }
 
   startup {
@@ -45,6 +76,32 @@ resource "proxmox_virtual_environment_vm" "vm" {
 
   network_device {
     bridge = "vmbr0"
+  }
+}
+
+resource "null_resource" "configure_vm" {
+
+  depends_on = [proxmox_virtual_environment_vm.vm]
+
+  connection {
+    type = "ssh"
+    user = "herder"
+    #private_key = file("./ssh/id_rsa")
+    private_key = tls_private_key.vm_key.private_key_pem
+    host = proxmox_virtual_environment_vm.vm.ipv4_addresses
+    timeout = "1m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "apk update",
+      "apk upgrade --no-cache",
+      "apk add --no-cache qemu-guest-agent net-tools openssh",
+      "rc-status",
+      "touch /run/openrc/softlevel",
+      "rc-update add sshd default",
+      "rc-service sshd start"
+    ]
   }
 }
 
@@ -59,9 +116,9 @@ resource "tls_private_key" "vm_key" {
   rsa_bits = 2048
 }
 
-output "vm_ip" {
-  value = proxmox_virtual_environment_vm.vm.ipv4_addresses
-}
+#output "vm_ip" {
+#  value = proxmox_virtual_environment_vm.vm.ipv4_addresses[0][0]
+#}
 
 output "vm_password" {
   value = random_password.vm_password.result
