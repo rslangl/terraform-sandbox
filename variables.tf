7f34061ec456d6d8d8d29a3d81434a5da4f5b05e
rslangl <fjellape1447@protonmail.com>
2025-03-25 19:33:17 +0100

    feat: add map-structure that contains workspace endpoint configs

--- variables.tf
+++ variables.tf
@@ -1,14 +1,24 @@
-variable "pve_endpoint" {
-  type = string
-  default = "https://192.168.88.2:8006/"
+variable "pve_config" {
+  type = map(object({
+      endpoint = string
+      username = string
+      password = string
+      node_name = string
+    }))
+  description = "Proxmox VE connection configuration"
 }
 
-variable "pve_username" {
-  type = string
-  default = "root@pam"
-}
-
-variable "pve_password" {
-  type = string
-  sensitive = true
-}
+#variable "pve_endpoint" {
+#  type = string
+#  default = "https://192.168.88.2:8006/"
+#}
+#
+#variable "pve_username" {
+#  type = string
+#  default = "root@pam"
+#}
+#
+#variable "pve_password" {
+#  type = string
+#  sensitive = true
+#}
